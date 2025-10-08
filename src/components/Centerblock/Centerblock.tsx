914fd577c4c1526555d5f09ea591ff4f38f00aed
Olga P. <zjkz14@gmail.com>
2025-10-08 20:15:26 +0300

    доработка

--- src/components/Centerblock/Centerblock.tsx
+++ src/components/Centerblock/Centerblock.tsx
@@ -4,13 +4,14 @@ import classnames from 'classnames';
 import Search from '../Search/Search';
 import { data } from '@/data';
 import { formatTime } from '@/utils/helper';
+import Filter from '../Filter/Filter';
 
 export default function Centerblock() {
   return (
     <div className={styles.centerblock}>
       <Search />
       <h2 className={styles.centerblock__h2}>Треки</h2>
-      
+      <Filter />
       <div className={styles.centerblock__content}>
         <div className={styles.content__title}>
           <div className={classnames(styles.playlistTitle__col, styles.col01)}>
@@ -59,7 +60,9 @@ export default function Centerblock() {
                   <svg className={styles.track__timeSvg}>
                     <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
                   </svg>
-                  <span className={styles.track__timeText}>{formatTime(track.duration_in_seconds)}</span>
+                  <span className={styles.track__timeText}>
+                    {formatTime(track.duration_in_seconds)}
+                  </span>
                 </div>
               </div>
             </div>
